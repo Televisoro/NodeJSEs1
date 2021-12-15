8d83514134f9347d7862d5ba33686d2811a688b2
galitom <galimberti.tommaso@studenti.salesianisesto.it>
2021-12-15 21:02:28 +0100

    Before test

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var express = require("express");
-var apiServer = express(); 
-
-console.log("yes");
-var a = 5;
-var b = "3";
-console.log(a+b);
+var apiServer = express();
+var fs = require("fs");
 
 var port=3000;
 var host="localhost";
@@ -14,13 +10,13 @@ apiServer.listen(port, host, ()=>{
 
 apiServer.get("/", (request, response)=>{
     console.log("richiesta GET /", request);
-    response.send("<h1>Ciao client sei in home</h1>");
+    response.send("<h1>Sei in home</h1>");
 });
 
 var cognome = "Galimberti";
 apiServer.get("/nome", (request, response)=>{
     console.log("richiesta GET /nome");
-    response.send("Ciao, il mio cognome è: "+cognome);
+    response.send("Il mio cognome è: "+cognome);
 });
 
 apiServer.get("/mioNome", (request, response)=>{
@@ -28,22 +24,45 @@ apiServer.get("/mioNome", (request, response)=>{
     response.send('Ciao, il tuo nome è: ' + request.query.nome);
 });
 
-//somma dei nomi 
+// https://localhost:3000/student?a=1&b=2
 apiServer.get("/somma", (request, response)=>{
-    console.log("request somma", request.query);
+    console.log("somma request", request.query);
     var a=parseInt(request.query.a);
     var b=parseInt(request.query.b);
     response.send('Risultato: ' + (a+b));
 });
 
+// https://localhost:3000/student?id=1
 apiServer.get("/student", (request, response)=>{
     console.log("student id: ", request.query.id);
-    FileSystem.readFile("studenti.json", (err, data) => {
-        if(err){
+
+    // Lettura file
+    fs.readFile("studenti.json", (err, data) => {
+        if (err) {
             console.log("error: "+err);
-        }else{
-            var studets = JSON.parse(data);
-            console.log("Students: "+students[0].surname);
+        } else {
+            var students = JSON.parse(data);
+            console.log(students[(request.query.id)-1]);
+            response.send(students.find(x => x.id === request.query.id));
+        }
+    });
+});
+
+apiServer.get("/newStudent", (request, response)=>{
+    console.log("new student: ", request.query.surname, request.query.name, request.query.id);
+    
+    // Write on a file
+    fs.readFile("studenti.json", (err, data) => {
+        if (err) {
+            console.log("error: "+err);
+        } else {
+            var students = JSON.parse(data);
+            students.push({"surname":request.query.surname,"name":request.query.name,"id":request.query.id});
+            fs.writeFile("studenti.json", JSON.stringify(students), (err) => {
+                if (err) {
+                    console.log("error: "+err);
+                }
+            });
         }
     });
 });
